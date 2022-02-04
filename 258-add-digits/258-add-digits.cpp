e7b91b09af9e5a0f873894d9c5b2808103febd5f
Debashish kumar sahoo <56837936+Debashish-hub@users.noreply.github.com>
2022-02-04 19:01:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.02%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int ans = 0;
+        while(num > 9){
+            while(num != 0){
+                ans += num % 10;
+                num /= 10;
+            }
+            num = ans;
+            ans = 0;
+        }
+        return num;
+    }
+};
