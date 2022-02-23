7847ec397a7231a6a4aaa6592f35c99b1ce590e7
Debashish kumar sahoo <56837936+Debashish-hub@users.noreply.github.com>
2022-02-23 06:30:36 -0800

    Time: 808 ms (67.22%), Space: 125.2 MB (37.67%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    static bool comp(vector<int> &x, vector<int> &y){
+        if(x[0] == y [0])return x[1] < y[1];
+
+        return x[0] > y[0];
+    }
+    int numberOfWeakCharacters(vector<vector<int>>& a) {
+        sort(a.begin(), a.end(), comp);
+        int maxe = INT_MIN;
+        int ans = 0;
+        int size = a.size();
+        for(int i=0; i <= size-1; i++){
+            if(a[i][1] < maxe){
+                ans++;
+            }
+            maxe = max(maxe, a[i][1]);
+        }
+        return ans;
+    }
+};
