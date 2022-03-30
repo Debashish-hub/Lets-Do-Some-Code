71b11af1049c46c6dd7ae4c2beff1a854cd0e90c
Debashish kumar sahoo <56837936+Debashish-hub@users.noreply.github.com>
2022-03-30 07:08:16 -0700

    Added solution - LeetHub

--- Print all nodes that don't have sibling - GFG/print-all-nodes-that-dont-have-sibling.cpp
+++ Print all nodes that don't have sibling - GFG/print-all-nodes-that-dont-have-sibling.cpp
@@ -112,18 +112,18 @@ int main()
      Node* left, *right;
 }; */
 void solve(Node* node,vector<int> &v){
-   if(node->left==NULL && node->right==NULL){
+   if(node->left == NULL && node->right == NULL){
        return;
    }
    
-   if(node->left==NULL && node->right!=NULL){
+   if(node->left == NULL && node->right != NULL){
        v.push_back(node->right->data);
        solve(node->right,v);
    }
-   else if(node->left!=NULL && node->right==NULL){
+   else if(node->left != NULL && node->right == NULL){
        v.push_back(node->left->data);
        solve(node->left,v);
-   }else if(node->left!=NULL && node->right!=NULL){
+   }else if(node->left != NULL && node->right != NULL){
        solve(node->left,v);
        solve(node->right,v);
    }
@@ -131,11 +131,11 @@ void solve(Node* node,vector<int> &v){
 vector<int> noSibling(Node* node)
 {
     // code here
-    vector<int> v;
-    solve(node,v);
-    sort(v.begin(),v.end());
-    if(v.size()==0){
-        v.push_back(-1);
+    vector<int> ans;
+    solve(node, ans);
+    sort(ans.begin(), ans.end());
+    if(ans.size()==0){
+        ans.push_back(-1);
     }
-    return v;
+    return ans;
 }
