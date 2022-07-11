7bded353a24438c4fd4a8f319c8a80a01b0a787e
Debashish kumar sahoo <56837936+Debashish-hub@users.noreply.github.com>
2022-07-11 07:40:31 +0530

    Added solution - LeetHub

---
+++ Reaching the heights - GFG/reaching-the-heights.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> reaching_height(int n, int a[]) ;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t-- > 0) {
+	    int n; 
+	    cin >> n;
+	    int a[n];
+	    for (int i = 0; i < n; i++) 
+	        cin >> a[i];
+	    vector<int> v;
+	    v = reaching_height(n,a);
+	
+	    if(v.size()==1 and v[0] == -1){
+	        cout << "Not Possible\n";
+	    }
+	    else{
+	    for(int i:v)
+	        cout << i << " ";
+	    cout << endl;}
+	    
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+//User function Template for C++
+
+vector<int> reaching_height(int n, int a[]) {
+   sort(a,a+n);
+   vector<int> ans;
+   
+   int left = 0, right = n-1;
+   int upstair = 0, downstair = 0;
+   
+   while(left <= right){
+       
+       if(left == right) ans.push_back(a[right--]);
+       else if(a[left] <= a[right]){
+           
+           upstair += a[right];
+           downstair += a[left];
+           
+           if(upstair == downstair) return { -1 };
+           
+           ans.push_back( a[right--] );
+           ans.push_back( a[left++] );
+       }
+       
+   }
+   return ans;
+}
